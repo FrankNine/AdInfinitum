0f56e3117c4847b9e59208b901c4d95d6727839f
Frank Chao <aaefiikmnnnr@gmail.com>
2018-04-13 16:06:50 +0800

    feat: Test scene for testing Unity StartCoroutine behaviour

---
+++ Assets/Scripts/UnityCoroutineTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+
+using UnityEngine;
+
+public class UnityCoroutineTest : MonoBehaviour {
+
+    private IEnumerator Start()
+    {
+        _LogWithTime("Start enter");
+        yield return StartCoroutine(_ProcessA());
+        _LogWithTime("Start exit");
+    }
+
+    private IEnumerator _ProcessA()
+    {
+        _LogWithTime("A enter");
+        yield return StartCoroutine(_ProcessB());
+        _LogWithTime("A exit");
+    }
+
+    private IEnumerator _ProcessB()
+    {
+        _LogWithTime("B enter");
+        yield break;
+    }
+
+    private void _LogWithTime(string message)
+    {
+        Debug.LogFormat("{0}: {1}", Time.realtimeSinceStartup, message);
+    }
+}
